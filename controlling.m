38cb1fc3f581f98ff0ba2c08154714a66e8d4a57
khkim95 <khkim95@vt.edu>
2018-05-04 19:17:20 -0400

    Update controlling.m

--- controlling.m
+++ controlling.m
@@ -1,4 +1,4 @@
-function r=controlling(NR)
+function r=controlling(NR, NC)
 %CONTROLLING determine the array of indices of Bounding boxes of interest.
 %   R=CONTROLLING(NR) outputs the row vector R containing the indices of
 %   the bounding boxes of interest from the matrix NR. NR is the matrix of
@@ -11,7 +11,7 @@ function r=controlling(NR)
 %   same y-coordinates are obtained.
 
 [Q,W]=hist(NR(:,4)); % Histogram of the y-dimension widths of all boxes.
-ind=find(Q==7); % Find indices from Q corresponding to frequency '6'.
+ind=find(Q==NC); % Find indices from Q corresponding to frequency '6'.
 % Since the number plates of cars in Karachi have six characters so
 % find(Q==6) is used. If the code is to be implemented for some other plates
 % the argument to the function 'find' has to be changed accordingly.
@@ -24,7 +24,7 @@ for k=1:length(NR)            % Taking the advantage of uniqueness of y-co
 end
 NR2=cat(2,NR,C_5');           % Appending new coloumn in NR.
 [E,R]=hist(NR2(:,5),20);
-Y=find(E==7);                 % Searching for six characters.
+Y=find(E==NC);                 % Searching for six characters.
 if length(ind)==1 % If six boxes of interest are succesfully found record
     MP=W(ind);    %  the midpoint of corresponding bin.
     binsize=W(2)-W(1); % Calculate the container size.
@@ -37,18 +37,18 @@ elseif length(Y)==1
     r=takeboxes(NR2,container,2.5); % Call to function takeboxes.    
 elseif isempty(ind) || length(ind)>1 % If there is no vlaue of '6' in the Q vector.
     [A,B]=hist(NR(:,2),20); % Use y-coordinate approach only.
-    ind2=find(A==7);
+    ind2=find(A==NC);
     if length(ind2)==1
         MP=B(ind2);
         binsize=B(2)-B(1);
         container=[MP-(binsize/2) MP+(binsize/2)]; % Calculating the complete container size.
         r=takeboxes(NR,container,1);
     else
-        container=guessthesix(A,B,(B(2)-B(1))); % Call of function guessthesix.
+        container=guessthesix(A,B,(B(2)-B(1)), NC); % Call of function guessthesix.
         if ~isempty(container) % If guessthesix works succesfully.
             r=takeboxes(NR,container,1); % Call the function takeboxes.
         elseif isempty(container)
-            container2=guessthesix(E,R,(R(2)-R(1)));
+            container2=guessthesix(E,R,(R(2)-R(1)), NC);
             if ~isempty(container2)
                 r=takeboxes(NR2,container2,2.5);
             else
